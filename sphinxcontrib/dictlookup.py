ff9734fdcd2f40ae2fae3bfedc9ed6336a5d5a26
jokullah <162468001+jokullah@users.noreply.github.com>
2025-05-20 16:07:29 +0000

    Update dictlookup.py

--- sphinxcontrib/dictlookup.py
+++ sphinxcontrib/dictlookup.py
@@ -2,6 +2,7 @@ import json
 import logging
 import sys
 from pathlib import Path
+import requests
 
 from sphinxcontrib.utils import configure_logger
 
@@ -20,9 +21,20 @@ def lookup(word):
     word = word.lower()
     # If 'word' is in citation form, look up in 'minstae' returns the translation.
     try:
-        # test test
+        url = "https://idord.arnastofnun.is/d/api/es/terms/?ord=" + word
+        response = requests.get(url)
+        response.raise_for_status()  # Raises HTTPError if the response was an error
+        data = response.json()
+        english_words = []
+
+        for key in data.get("results", []):
+            for word_info in key.get("words", []):
+                if word_info.get("fklanguage") == "EN":
+                    english_words.append(word_info.get("word"))
+
+        
         entry = minstae_data[word]
-        entry["enTerm"] = 'test'
+        entry["enTerm"] = english_words
         entry["isTerm"] = word
         return entry
     # If 'word' is not found in 'minstae', it may not be in citation form and a
